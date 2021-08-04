7596686cb103d09de51f29d791c04b1adf7e1b21
VisualSource <boomishere_network@outlook.com>
2021-08-04 17:27:13 -0400

    status updates

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use chrono::{Utc};
 use json::JsonValue;
 use std::fs::read_to_string;
 use std::fs::write;
+use std::collections::hash_map;
 
 fn load_config() -> Result<JsonValue,String> {
     match read_to_string("./config.json") {
@@ -98,6 +99,16 @@ fn read_current_dns(debug: bool) -> Result<JsonValue,String> {
 }
 
 fn main() -> Result<(), String> {
+    let mut request_status = hash_map::HashMap::<String,String>::new();
+    request_status.insert("nohost".to_string(),"The hostname doesn't exist, or doesn't have Dynamic DNS enabled.".to_string());
+    request_status.insert("badauth".to_string(),"The username/password combination isn't valid for the specified host.".to_string());
+    request_status.insert("notfqdn".to_string(),"The supplied hostname isn't a valid fully-qualified domain name.".to_string());
+    request_status.insert("badagent".to_string(),"Your Dynamic DNS client makes bad requests. Ensure the user agent is set in the request.".to_string());
+    request_status.insert("abuse".to_string(),"Dynamic DNS access for the hostname has been blocked due to failure to interpret previous responses correctly.".to_string());
+    request_status.insert("911".to_string(),"An error happened on our end. Wait 5 minutes and retry.".to_string());
+    request_status.insert("conflict A".to_string(),"A custom A or AAAA resource record conflicts with the update. Delete the indicated resource record within the DNS settings page and try the update again.".to_string());
+    request_status.insert("conflict AAAA".to_string(),"A custom A or AAAA resource record conflicts with the update. Delete the indicated resource record within the DNS settings page and try the update again.".to_string());
+
     let config = load_config().unwrap();
     let is_debug = config["debug"].as_bool().unwrap();
 
@@ -105,19 +116,37 @@ fn main() -> Result<(), String> {
    
     let ip = get_exteral_ip(is_debug).unwrap();
 
+    request_status.insert(format!("good {}",ip.clone()).to_string(), String::from("The update was successful. You should not attempt another update until your IP address changes."));
+    request_status.insert(format!("nochg {}",ip.clone()).to_string(), String::from("The supplied IP address is already set for this host. You should not attempt another update until your IP address changes."));
+
+
     for key in config["domains"].members() {
         let domain_name = key["domain"].to_string();
         let domain = &dns[domain_name.clone()];
         if domain["ip"] != ip {
-            dns[domain_name] = object!{
+            dns[domain_name.clone()] = object!{
                 ip: ip.clone(),
                 changed: Utc::now().to_rfc2822()
             };
-            if is_debug { println!("IPs differ"); }
+            if is_debug { println!("IP of {} does not match exterial",domain_name.clone()); }
         }
         let request = create_request(&key ,&ip, is_debug);
         if !is_debug { 
-            reqwest::blocking::get(request).unwrap(); 
+            match reqwest::blocking::get(request) {
+                Ok(value) => {
+                    match value.text() {
+                        Ok(result) => {
+                            println!("{}",request_status[&result]);
+                        }
+                        Err(parse_err) => {
+                            eprintln!("HTTP UPDATE REQUEST ERROR: {}",parse_err);
+                        }
+                    }
+                }
+                Err(err) => {
+                    eprintln!("HTTP UPDATE REQUEST ERROR: {}",err);
+                }
+            }
         }
     }
 
